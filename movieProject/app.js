8c45b0080c82835a631b8111b167354e08b43701
Kwak221 <113527506+Kwak221@users.noreply.github.com>
2024-04-10 22:28:43 -0400

    Docker

--- movieProject/app.js
+++ movieProject/app.js
@@ -3,11 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors())
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
